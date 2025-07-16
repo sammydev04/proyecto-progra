99aeeb8c084a1d509d22e0bff50c558473d55116
MarioCg93 <macog19@gmail.com>
2025-07-15 23:24:02 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,1 +1,16 @@
-print("Hello World")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("+                                                           +")
+print("+ ¡Bienvenidos al sistema de control de gastos personales!  +")
+print("+                                                           +")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print ("")
+user =input("Por favor ingresa tu nombre de usuario: ")
+
+if user == "S_Chacon":
+    print("Ingresa tu contraseña: ")
+elif user == "K_Fallas":
+    print("Ingresa tu contraseña: ")
+elif user == "M_Cordero":
+    print("Ingresa tu contraseña: ")
+else:
+    print("Usuario inválido. ")
